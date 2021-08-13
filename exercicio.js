889fac2d8408f4f9b7dcab27ebb07ec0f13d67f7
leonardo gomes <leonardogomespadilha@gmail.com>
2021-08-13 15:55:28 -0300

    Reformula o código e adiciona funções

--- exercicio.js
+++ exercicio.js
@@ -1,28 +1,37 @@
 const fs = require("fs")
-
 const database = fs.readFileSync('./novo-arquivo2', `utf-8`)
-
 const leitura = database.split("\n")
-//console.log(leitura)
-//Nome:
-//Tipo:
-//Falta quantos niveis para evoluir? 
-    let objeto;
-for(i= 0; i< leitura.length; i++){
-    //console.log(leitura[i])
-    objeto = leitura[i].split('-')
-     let evoluindo = parseInt(objeto[1]) - 16
-    console.log("Nome: " + objeto[0] + "- Nível" + objeto[1] + "\nTipo: " + objeto[2] + "Ataque: " + objeto[3] + `\nFalta quantos níveis para evoluir?: ${evoluindo} ` + evoluir())
+//console.log(leitura.length)
 
-    function evoluir(){
-        if(objeto[4]>= evoluindo){
-            console.log("\n"+"Evolui para "+objeto[5])
-        } else {
-            console.log("Nao evoluiu para seu proximo estagio.")
-        }
-        
+function pikachu() {  
+    const objeto = ["", "", "", "", ""];     
+    if (objeto[4].indexOf("evolui com pedra")) {
+        console.log("Evolui com pedra!")
     }
+}
 
+function imprimir(objeto) {
+
+    //console.log("Nome: " + objeto[0] + " - Nível" + objeto[1])
+    //console.log("Tipo: " + objeto[2] + "/ Ataque: " + objeto[3])
+    //console.log(`Falta quantos níveis para evoluir? ${evoluindo}` + '\n')
+    
+}
+
+//  nome: obterNivel
+// entrada: Nv1000
+// saida: 1000
+function obterNivel(objeto){
+    objeto[1].substring(1)
+    console.log(objeto[1])
+}
+
+let objeto;
+let evoluindo;
+for (i = 0; i < leitura.length; i++) {
+    objeto = leitura[i].split('-')
+    imprimir(objeto)   
+    obterNivel(objeto)    
 }
 
 
