c2be5c56a62ed25b7e5c2eeb70ce111ed87e1b7c
Jorel Martins <bearshamis@gmail.com>
2021-08-13 13:47:34 -0300

    pokemons

--- exercicio.js
+++ exercicio.js
@@ -7,23 +7,23 @@ const leitura = database.split("\n")
 //Nome:
 //Tipo:
 //Falta quantos niveis para evoluir? 
-    var objeto;
+    let objeto;
 for(i= 0; i< leitura.length; i++){
     //console.log(leitura[i])
     objeto = leitura[i].split('-')
-    let evoluindo = parseFloat.objeto[1] - 13
-    console.log("Nome: " + objeto[0] + "- Nível" + objeto[1] + "\nTipo: " + objeto[2] + "Ataque: " + objeto[3] + `\nFalta quantos níveis para evoluir?: ${evoluindo} `)
+     let evoluindo = parseInt(objeto[1]) - 16
+    console.log("Nome: " + objeto[0] + "- Nível" + objeto[1] + "\nTipo: " + objeto[2] + "Ataque: " + objeto[3] + `\nFalta quantos níveis para evoluir?: ${evoluindo} ` + evoluir())
+
+    function evoluir(){
+        if(objeto[4]>= evoluindo){
+            console.log("\n"+"Evolui para "+objeto[5])
+        } else {
+            console.log("Nao evoluiu para seu proximo estagio.")
+        }
+        
+    }
+
 }
-function evolucao(){
-   if (objeto[1] < 16 objeto[4]){
-       objeto[1] - objeto[4]
-
-
-   }else{
-
-   }
-}
-
 
 
 
