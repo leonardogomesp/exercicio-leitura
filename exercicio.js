9379bb7eb52400d6ca4bf07286a3eade48eb592c
gustavo <muchira003@gmail.com>
2021-08-16 11:20:42 -0300

    cria funcao trocarletra

--- exercicio.js
+++ exercicio.js
@@ -3,25 +3,29 @@ const database = fs.readFileSync('./novo-arquivo2', `utf-8`)
 const leitura = database.split("\n")
 //console.log(leitura.length)
 
-function pikachu(objeto) {   
-    if (objeto[0].indexOf("pikachu")) {
-        console.log("Evolui com pedra!")
-    }
+function trocarLetra(palavra){
+return palavra[0].toUpperCase() + palavra.slice(1).toLowerCase()
+
 }
 
 function imprimir(objeto) {
 
-    console.log("Nome: " + objeto[0] + " - Nível " + obterNivel(objeto)) 
+    console.log("Nome: " + trocarLetra(objeto[0]) + " - Nível " + obterNivel(objeto)) 
     console.log("Tipo: " + objeto[2] + "/ Ataque: " + objeto[3])
     console.log(`Falta quantos níveis para evoluir? ` + faltaEvoluir(objeto) + "\n")
     
 }
 function faltaEvoluir(objeto){
+    if (objeto[0].trim() === "pikachu") {
+        return "Evolui com pedra!"
+    }
     return 16 - obterNivel(objeto)
+
 } 
 //  nome: obterNivel
 // entrada: Nv1000
 // saida: 1000
+
 function obterNivel(objeto){
     return parseInt(objeto[1].substring(3))
 }
