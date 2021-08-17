b3367a6885eac4b6d2ffbeb3c638027d41f515cc
leonardo gomes <leonardogomespadilha@gmail.com>
2021-08-17 15:47:50 -0300

    Adiciona explicações ao codigo

--- exercicio.js
+++ exercicio.js
@@ -1,28 +1,36 @@
-const evoluiComPedra = require('./pokemon-evolucao-pedra') 
 // A variavel evoluiComPedra esta recebendo o arquivo pokemon-evolucao-pedra atraves da funcao require
-const fs = require("fs")
+const evoluiComPedra = require('./pokemon-evolucao-pedra')
 // A variavel fs esta recebendo da funcao require o modulo fs
+const fs = require("fs")
+// A variavel database está recebendo a variavel fs e lendo o novo-arquivo2
 const database = fs.readFileSync('./novo-arquivo2', 'utf-8')
-
+// A variavel leitura está recebendo a variavel database e separando pela quebra de linha
 const leitura = database.split("\n")
-console.log({leitura, database})
-/*
+
+// A função primeiraLetraMaiuscula é criada
 function primeiraLetraMaiuscula(palavra) {
-    
+    // A variavel palavraSemEspaco está recebendo palavra e retirando seus espaços com o .trim ()
     const palavraSemEspaco = palavra.trim()
+    // Está sendo retornado uma palavra com inicial maiuscula 
+    // O método toUpperCase é responsável por transofrmar em maiusculo
+    // O método toLowerCase é responsável por transformar em minusculo
+    // O método slice é responsável por dividir a string a partir de um indice
+    // Todos estes métodos são encontrados dentro de qualquer string
     return palavraSemEspaco[0].toUpperCase() + palavraSemEspaco.slice(1).toLowerCase()
 }
 
-
-function faltaEvoluir(objeto) {
-    if (evoluiComPedra.includes(objeto[0].trim())) {
+// A função faltaEvoluir é criada
+function faltaEvoluir(pokemon) {
+    // Estabelece um if que verifica se a nossa entrada é igual as informações do banco de dados
+    // O método includes procura a nossa entrada dentro de pokemon-evolucao-pedra,
+    // se positivo retorna true, se negativo, retorna false. 
+    if (evoluiComPedra.includes(pokemon[0].trim())) {
+        // Se nosso if receber true, retornará a string "Evolui com pedra!"
         return "Evolui com pedra!"
     }
-   
-    return 16 - obterNivel(objeto)
-
+    // Se nosso if receber false, retornará quantos niveis restam para evoluir.
+    return 16 - obterNivel(pokemon)
 }
-
 
 function obterNivel(pokemon) {
     return parseInt(pokemon[1].substring(3))
@@ -47,9 +55,9 @@ function main(pokemon) {
         pokemon = leitura[i].split('-')
         imprimir(pokemon)
     }
+    for (i = 0; i < evoluiComPedra.length; i++) {
+        pokemon = evoluiComPedra[i]
+    }
+}
 
-}
-for (i = 0; i < evoluiComPedra.length; i++) {
-    pokemon = evoluiComPedra[i]
-}
-return main()*/
+main()
